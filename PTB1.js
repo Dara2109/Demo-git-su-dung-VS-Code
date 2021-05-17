7b48a4e4e6a0391dfe5aa1bdb4ea2655d231e2dd
Dara2109 <hatde2109@gmail.com>
2021-05-17 19:08:37 +0700

    PTB1

---
+++ PTB1.js
@@ -0,0 +1,11 @@
+let a=+prompt ("Nhap a:");
+let b=+prompt ("Nhap b:");
+If (a==0)
+    {If (b==0)
+    alert ("PT co vo so nghiem")};
+else
+    alert ("PT vo nghiem")};
+}else{
+    x=-b/a;
+    alert ("PT co nghiem la:" +x);
+}
